d16f2d0892d264c0bfe97f5d4a73f5d723851b86
SanOtaku09 <sanotaku098@gmail.com>
2022-05-28 23:00:06 +0530

    make normal function

--- algo/src/main/kotlin/string/longestPalindromicSubstring/longestPalindromicSubstring.kt
+++ algo/src/main/kotlin/string/longestPalindromicSubstring/longestPalindromicSubstring.kt
@@ -50,16 +50,17 @@ object longestPalindromicSubstring {
 
     // dynamic ans without recursion
     var len = 0 ; var length = 0 ; var init = 0
-    fun String.longestPalidromeSubstringWithLoop() : String {
-        val array = this.toCharArray()
-        len = this.length
-        if ( len <= 1 ) return  this
+    fun longestPalidromeSubstringWithLoop( s : String ) : String {
+        var s = s
+        len = s.length
+        if ( len <= 1 ) return s
+        val array = s.toCharArray()
         var i = 0
         while ( i < len ) {
-            i = manacher( array , i )
+            i = manacher(array, i)
             i++
         }
-        return this.substring( init , init + length )
+        return s.substring( init , init + length )
     }
 
     fun manacher( array : CharArray , k : Int ) : Int {
@@ -72,7 +73,7 @@ object longestPalindromicSubstring {
         }
         if ( j - i - 1 > length ) {
             length = j - i - 1
-            init = i +1
+            init = i + 1
         }
         return netcentre
     }
