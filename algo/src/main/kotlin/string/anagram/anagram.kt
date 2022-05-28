6287208e2032cf7a7ce029994bd98e3b55070ef4
SanOtaku09 <sanotaku098@gmail.com>
2022-05-28 11:30:52 +0530

    ignore cases

--- algo/src/main/kotlin/string/anagram/anagram.kt
+++ algo/src/main/kotlin/string/anagram/anagram.kt
@@ -5,8 +5,8 @@ object anagram {
 
     fun String.isANagramOf( s : String ) : Boolean {
         if ( this.length != s.length ) return false
-        val s1 = this.toCharArray()
-        val s2 = s.toCharArray()
+        val s1 = this.lowercase().toCharArray()
+        val s2 = s.lowercase().toCharArray()
         Arrays.sort( s1 )
         Arrays.sort( s2 )
         return Arrays.equals( s1 , s2 )
