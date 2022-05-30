9d4a6b59f70e8ed77c92bee18faca5bb7c11f9dc
SanOtaku09 <sanotaku098@gmail.com>
2022-05-31 01:33:14 +0530

    add more test to keyboard test

--- algo/src/test/kotlin/string/KeyboardRow/KeyboardRowTest.kt
+++ algo/src/test/kotlin/string/KeyboardRow/KeyboardRowTest.kt
@@ -8,5 +8,8 @@ class KeyboardRowTest {
     @Test
     fun test() {
         Assert.assertArrayEquals( findWords( arrayOf( "Hello","Alaska","Dad","Peace" ) ) , arrayOf( "Alaska","Dad" ) )
+        Assert.assertArrayEquals( findWords( arrayOf( "omk" ) ) , Array( 0 ){ ""} )
+        Assert.assertArrayEquals( findWords( arrayOf( "adsdf","sfd" ) ) , arrayOf( "adsdf","sfd" ) )
+
     }
 }
