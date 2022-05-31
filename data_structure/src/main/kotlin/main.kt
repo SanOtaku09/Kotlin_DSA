69542f48f72d29de8f47cbf5bd80afef2e2ae815
SanOtaku09 <sanotaku098@gmail.com>
2022-05-31 14:17:37 +0530

    open test in default browser in supported os

--- data_structure/src/main/kotlin/main.kt
+++ data_structure/src/main/kotlin/main.kt
@@ -1,5 +1,20 @@
 @file : JvmName( "main" )
+import java.awt.Desktop
+import java.net.URI
 fun main() {
     Runtime.getRuntime().exec( "gradle test" ).waitFor()
-    Runtime.getRuntime().exec( "xdg-open ${System.getProperty( "user.dir" )}/build/reports/tests/test/index.html")
+    "${System.getProperty( "user.dir" )}/build/reports/tests/test/index.html".let { url ->
+        val osName = System.getProperty("os.name").lowercase()
+        if ( osName.contains( "mac" ) ) {
+            Runtime.getRuntime().exec( "open $url" )
+        } else if ( osName.contains( "nix" ) || osName.contains("nux") ) {
+            Runtime.getRuntime().exec("xdg-open $url")
+        } else {
+            try {
+                Desktop.getDesktop().browse( URI( url ) )
+            } catch ( notRecognised : Exception ){
+                println( " $osName : unknown os" )
+            }
+        }
+    }
 }
