a049fd7572ff438ea1f83d273e774df55b661e86
sanotaku09 <sanotaku098@gmail.com>
2022-06-02 23:58:24 +0530

    Length of Last Word

--- algo/src/main/kotlin/string/LengthOfLastWord/LengthOfLastWord.kt
+++ algo/src/main/kotlin/string/LengthOfLastWord/LengthOfLastWord.kt
@@ -3,9 +3,9 @@ package string.LengthOfLastWord
 object LengthOfLastWord {
     fun lengthOfLastWord(s: String): Int {
         var i = s.length -1
-        while ( i > 0 && s[i] == ' ' ) i--
+        while ( i >= 0 && s[i] == ' ' ) i--
         var len = 0
-        while ( i > 0 && s[i--] != ' ' ) len++
+        while ( i >= 0 && s[i--] != ' ' ) len++
         return len
     }
 }
